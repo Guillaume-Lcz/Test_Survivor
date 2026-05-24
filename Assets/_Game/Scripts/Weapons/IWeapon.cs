1a996cf82523694f9cfa480991623e9eef643414
guillaume_lcz <guillaume.le-coz@epitech.eu>
2026-05-25 00:06:08 +0200

    ADD: weapon stats interface (Damage, FireRateBonus, ProjectileSpeed, Range, ProjectileCount)

--- Assets/_Game/Scripts/Weapons/IWeapon.cs
+++ Assets/_Game/Scripts/Weapons/IWeapon.cs
@@ -1,4 +1,9 @@
 public interface IWeapon
 {
     void Activate();
+    float Damage { get; set; }
+    float FireRateBonus { get; set; }
+    float ProjectileSpeed { get; set; }
+    float Range { get; set; }
+    int ProjectileCount { get; set; }
 }
